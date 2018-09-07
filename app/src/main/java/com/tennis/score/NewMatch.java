6abc2a1fa9b8014a129207dbc526808eeb3d30a6
Ali Rehman <rehmanaa@uci.edu>
2018-09-07 00:27:16 -0700

    Added new match layout and button

---
+++ app/src/main/java/com/tennis/score/NewMatch.java
@@ -0,0 +1,22 @@
+package com.tennis.score;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by Ali on 9/6/2018.
+ */
+
+public class NewMatch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_match_form);
+    }
+}
+
