65ef08652c0bc497b5bc41681c51d26c0be9c4a1
Ali Rehman <rehmanaa@uci.edu>
2018-09-08 03:00:40 -0700

    Added warm up timer and display

--- app/src/main/java/com/tennis/score/activity/PreMatchSetup.java
+++ app/src/main/java/com/tennis/score/activity/PreMatchSetup.java
@@ -3,6 +3,9 @@ package com.tennis.score.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.tennis.score.R;
 
@@ -22,5 +25,30 @@ public class PreMatchSetup extends AppCompatActivity {
         String matchFormat = intent.getStringExtra("matchFormat");
 
         System.out.println(matchName + "-" + playerOneName + "-" + playerTwoName + "-" + adRule + "-" + matchFormat);
+    }
+
+    public void startTimer(View view) {
+        double warmupTime = Double.parseDouble(((EditText)findViewById(R.id.warmupTime)).getText().toString());
+
+        int remainingSeconds = (int)(warmupTime * 60);
+
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+
+        String minuteString = String.valueOf(minutes);
+        String secondString = String.valueOf(seconds);
+
+        if (minuteString.length() == 1) {
+            minuteString = "0" + minuteString;
+        }
+        if (secondString.length() == 1) {
+            secondString = "0" + secondString;
+        }
+
+        ((TextView)findViewById(R.id.timerDisplay)).setText(minuteString + ":" + secondString);
+    }
+
+    public void tester(View view) {
+        System.out.println("test");
     }
 }
