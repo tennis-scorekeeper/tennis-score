dc1f023e006122f83621e53e625f0ebeee4819df
Ali Rehman <rehmanaa@uci.edu>
2018-09-20 12:50:49 -0700

    Implemented functionality to add other users to tournaments

--- app/src/main/java/com/tennis/score/activity/MatchList.java
+++ app/src/main/java/com/tennis/score/activity/MatchList.java
@@ -3,11 +3,15 @@ package com.tennis.score.activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,9 +26,14 @@ import org.json.JSONObject;
 
 public class MatchList extends AppCompatActivity {
 
+    private final int blackColor = 0xff000000;
+    private final int redColor = 0xffcc0000;
+    private final int greenColor = 0xff99cc00;
+
     String signedInEmail;
     int tournamentId;
     String tournamentName;
+    JSONObject tournamentObject;
 
     private View.OnClickListener viewMatch;
 
@@ -41,7 +50,9 @@ public class MatchList extends AppCompatActivity {
         signedInEmail = intent.getStringExtra("signedInEmail");
         tournamentId = intent.getIntExtra("tournamentId", -1);
 
+        setTournamentObject();
         setTournamentName();
+        hideOptions();
         createViewMatchOnClickListener();
         updateMatchList();
     }
@@ -82,6 +93,87 @@ public class MatchList extends AppCompatActivity {
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        builder.show();
+    }
+
+    public void addUserToTournament() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add chair umpire");
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final TextView resultMessage = new TextView(this);
+        final TextView inputLabel = new TextView(this);
+        final EditText input = new EditText(this);
+
+        resultMessage.setText("");
+        resultMessage.setGravity(1);
+        resultMessage.setPadding(0,25,0,25);
+
+        inputLabel.setText("Enter email of chair umpire");
+        inputLabel.setTextColor(blackColor);
+
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+
+        Button addButton = new Button(this);
+        addButton.setText("Add");
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String emailToAdd = input.getText().toString();
+
+                if (emailToAdd.length() <= 0) {
+                    resultMessage.setText("Please enter an email.");
+                    resultMessage.setTextColor(redColor);
+                    return;
+                }
+
+                try {
+                    String response = new RetrieveFeedTask(
+                            "https://www.mikenguyenmd.com/match_live/addUserTournamentMapping",
+                            new String[]{"email", emailToAdd, "tournamentId", String.valueOf(tournamentId)}).execute().get();
+                    if (response.equals("0")) {
+                        resultMessage.setText(emailToAdd + " added to this tournament");
+                        resultMessage.setTextColor(greenColor);
+                        return;
+                    }
+                    if (response.equals("1")) {
+                        resultMessage.setText("User with email " + emailToAdd + " does not exist");
+                        resultMessage.setTextColor(redColor);
+                        return;
+                    }
+                    else if (response.equals("2")) {
+                        resultMessage.setText(emailToAdd + " is already added to this tournament");
+                        resultMessage.setTextColor(redColor);
+                        return;
+                    }
+                    else {
+                        resultMessage.setText("An error occured while adding user.");
+                        resultMessage.setTextColor(redColor);
+                    }
+
+
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    resultMessage.setText("An error occured while adding user.");
+                    resultMessage.setTextColor(redColor);
+                }
+            }
+        });
+
+        layout.addView(resultMessage);
+        layout.addView(inputLabel);
+        layout.addView(input);
+        layout.addView(addButton);
+        builder.setView(layout);
+
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
             }
         });
@@ -130,7 +222,7 @@ public class MatchList extends AppCompatActivity {
         }
     }
 
-    private void setTournamentName() {
+    private void setTournamentObject() {
         String[] params = new String[]{
                 "tournamentId", String.valueOf(tournamentId)};
 
@@ -139,10 +231,29 @@ public class MatchList extends AppCompatActivity {
                     "https://www.mikenguyenmd.com/match_live/getTournament", params).execute().get();
 
             if (!response.equals("null")) {
-                JSONObject tournamentObject = new JSONObject(response.toString());
-                tournamentName = tournamentObject.getString("tournamentName");
-                ((TextView)findViewById(R.id.matchListTournamentNameDisplay)).setText(tournamentName);
+                tournamentObject = new JSONObject(response.toString());
             }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void hideOptions() {
+        try {
+            if (!signedInEmail.equals(tournamentObject.getString("creatorEmail"))) {
+                (findViewById(R.id.fab)).setVisibility(View.GONE);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setTournamentName() {
+        try{
+            tournamentName = tournamentObject.getString("tournamentName");
+            ((TextView)findViewById(R.id.matchListTournamentNameDisplay)).setText(tournamentName);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -152,7 +263,14 @@ public class MatchList extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tennis_score, menu);
+        try {
+            if (signedInEmail.equals(tournamentObject.getString("creatorEmail"))) {
+                getMenuInflater().inflate(R.menu.menu_tennis_score, menu);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
@@ -171,6 +289,10 @@ public class MatchList extends AppCompatActivity {
         if (id == R.id.deleteMatches) {
             System.out.println("calling dialog");
             deleteMatches();
+        }
+
+        if (id == R.id.addUserToTournament) {
+            addUserToTournament();
         }
 
         return super.onOptionsItemSelected(item);
